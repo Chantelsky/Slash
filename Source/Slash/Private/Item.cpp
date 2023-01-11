cf74a1bf0984f585fb743b44969352b9ffe66d6f
Chantel Punt <csky.punt@gmail.com>
2023-01-11 16:24:58 +1000

    tidy up and add draw debug lines

--- Source/Slash/Private/Item.cpp
+++ Source/Slash/Private/Item.cpp
@@ -2,25 +2,26 @@
 
 
 #include "Item.h"
+#include "DrawDebugHelpers.h"
 #include "Slash/Slash.h"
 
-// Sets default values
 AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
 	UWorld* World = GetWorld();
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	
 	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 }
 
-// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/Slash/Slash.h
+++ Source/Slash/Slash.h
@@ -4,5 +4,6 @@
 
 #include "CoreMinimal.h"
 
-/** debug macro**/
+/** debug macros **/
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
